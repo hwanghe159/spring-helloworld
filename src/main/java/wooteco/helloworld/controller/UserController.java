eb597088c1cbc795e3f23e06e4460f8623043ff2
황준호 <42054054+hwanghe159@users.noreply.github.com>
2020-04-25 22:46:38 +0900

    feat : mvc 실습 완료

--- src/main/java/wooteco/helloworld/controller/UserController.java
+++ src/main/java/wooteco/helloworld/controller/UserController.java
@@ -1,6 +1,6 @@
 package wooteco.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wooteco.helloworld.domain.User;
 
 import java.util.Map;
@@ -10,29 +10,33 @@ public class UserController {
     /**
      * GET /users
      */
-    public String sayHi(String name) {
+    @GetMapping("/users")
+    public String sayHi(String name) {//쿼리스트링으로 날린 정보는 인자로 받아서 처리. 앞에 @RequestParam붙여서 추가정보 나타낼수도 있음
         return "Hi " + name;
-    }
-
-    /**
-     * GET /users/{id}
-     */
-    public String retrieveUser(Long id) {
-        return "user's id is " + id;
     }
 
     /**
      * POST /users
      */
-    public String createUserWithJson(User user) {
+    @PostMapping("/users")
+    public String createUserWithJson(@RequestBody User user) {//body에 정보를 포함되어서 왔다면 이렇게 받아야 함
         return "Hello " + user.getName();
     }
 
     /**
      * POST /users/form
      */
-    public String createUserWithForm(Map<String, String> paramMap) {
+    @PostMapping("/users/form")
+    public String createUserWithForm(@RequestParam Map<String, String> paramMap) {//form형식으로 param에 담겨왔을때
         return "Hello " + paramMap.get("name");
+    }
+
+    /**
+     * GET /users/{id}
+     */
+    @GetMapping("/users/{id}")
+    public String retrieveUser(@PathVariable Long id) {//path로 받는 정보는 이렇게 처리
+        return "user's id is " + id;
     }
 
 }
--- src/main/java/wooteco/helloworld/controller/WelcomeController.java
+++ src/main/java/wooteco/helloworld/controller/WelcomeController.java
@@ -1,14 +1,20 @@
 package wooteco.helloworld.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import wooteco.helloworld.domain.User;
 
+@Controller//모든 메서드에 @ResponseBody가 붙으면 @RestController로 바꾸고 @ResponseBody제거 가능
 public class WelcomeController {
     /**
      * GET /welcome
      */
+    @GetMapping("/welcome")
+    @ResponseBody//얘를 붙임으로써 문자열로 응답할 수 있게됨
     public String welcome() {
         return "welcome";
     }
@@ -16,13 +22,16 @@ public class WelcomeController {
     /**
      * GET /welcome-page
      */
-    public String welcomePage() {
+    @GetMapping("/welcome-page")
+    public String welcomePage(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);//model에 add하여 프론트에 전달 가능
         return "welcome";
     }
 
     /**
      * GET /welcome-user
      */
+    @GetMapping("/welcome-user")
     public ResponseEntity welcomeUser() {
         return ResponseEntity.ok(new User("브라운", "brown@email.com", 20));
     }
